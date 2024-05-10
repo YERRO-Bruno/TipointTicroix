f6a50381b685c2400a19238d529ca59d9c48e9cf
YERRO-Bruno <brunoyerro@gmail.com>
2024-05-10 17:08:21 -0400

    sprint1

--- TIPOINTICROIX/urls.py
+++ TIPOINTICROIX/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from tipoint_ticroix import views
 urlpatterns = [
-    path('tipointticroix', views.tipointticroix, name='tipointticroix'),
+    path('', views.accueil, name='accueil'),
+    path('tipointticroix/', views.tipointticroix, name='tipointticroix'),
     path('admin/', admin.site.urls),
 ]
