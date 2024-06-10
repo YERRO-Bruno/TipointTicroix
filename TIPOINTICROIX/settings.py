78e390e46acd56b9e406e54e602bff41f2c77679
YERRO-Bruno <brunoyerro@gmail.com>
2024-06-10 17:39:18 -0400

    host 195.35.28.193

--- TIPOINTICROIX/settings.py
+++ TIPOINTICROIX/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         "NAME": os.getenv('NAME'),
         "USER": os.getenv('USER'),
         "PASSWORD": os.getenv('PASSWORD'),
-        #"HOST": os.getenv('HOST'),
+        "HOST": os.getenv('HOST'),
         'PORT': 3306,  
         'OPTIONS': {  
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
