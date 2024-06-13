bbad504e8903a8cb54339e564083750b9d5fa5f8
YERRO-Bruno <brunoyerro@gmail.com>
2024-06-13 12:01:04 -0400

    server.py

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 import asyncio
 import websockets
-import TIPOINTICROIX.settings
 
 async def echo(websocket, path):
     async for message in websocket:
-        print(f"Received message: {message}")
         await websocket.send(message)
 
-start_server = websockets.serve(echo, TIPOINTICROIX.settings.WEB_SOCKET_SERVER, 8765)
+async def main():
+    server = await websockets.serve(echo, '0.0.0.0', 8765)
+    print('WebSocket server is running on ws://0.0.0.0:8765')
+    await server.wait_closed()
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+if __name__ == "__main__":
+    asyncio.run(main())
