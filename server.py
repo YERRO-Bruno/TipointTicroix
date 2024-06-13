253c19089fb96ea1c15380ff3638c3a08a313256
YERRO-Bruno <brunoyerro@gmail.com>
2024-06-13 12:18:10 -0400

    server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ async def echo(websocket, path):
 
 async def main():
     server = await websockets.serve(echo, '195.35.28.193', 8765)
-    print('WebSocket server is running on ws://10.0.1.185:8765')
+    print('WebSocket server is running on ws://195.35.28.193')
     await server.wait_closed()
 
 if __name__ == "__main__":
