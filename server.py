63887e5b4e9b848138f3885bdd685c7c938b6407
YERRO-Bruno <brunoyerro@gmail.com>
2024-06-13 12:14:39 -0400

    server.py

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ async def echo(websocket, path):
         await websocket.send(message)
 
 async def main():
-    server = await websockets.serve(echo, '10.0.1.185', 8765)
+    server = await websockets.serve(echo, '195.35.28.193', 8765)
     print('WebSocket server is running on ws://10.0.1.185:8765')
     await server.wait_closed()
 
