f4eb6c71b60b4d70948f8619cebda479b466d0f1
YERRO-Bruno <brunoyerro@gmail.com>
2024-06-26 10:16:00 -0400

    server3

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn TIPOINTICROIX.wsgi --timeout 120 --workers=3 --threads=3 --worker-connections=1000
-worker: uvicorn TIPOINTICROIX.asgi:main --host 0.0.0.0 --port 8765
+worker: python TIPOINTICROIX.asgi.py
--- TIPOINTICROIX/asgi.py
+++ TIPOINTICROIX/asgi.py
@@ -7,7 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 import asyncio
-from server3 import main
+import websockets
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
+
+async def handler(websocket, path):
+    while True:
+        message = await websocket.recv()
+        print(message)
+
+async def main():
+    async with websockets.serve(handler, "0.0.0.0", 8765):
+        await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
     asyncio.run(main())
