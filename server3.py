9071ed1a4042937ecd1d34220cf67a4de45cd084
YERRO-Bruno <brunoyerro@gmail.com>
2024-06-26 09:07:28 -0400

    server4

--- server3.py
+++ server3.py
@@ -1,18 +1,13 @@
 import asyncio
-
 import websockets
-
-
 async def handler(websocket):
     while True:
         message = await websocket.recv()
         print(message)
 
-
 async def main():
     async with websockets.serve(handler, "", 8765):
         await asyncio.Future()  # run forever
-
 
 if __name__ == "__main__":
     asyncio.run(main())
