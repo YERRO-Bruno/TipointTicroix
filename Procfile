ca229758d63fb86f51e0af4d7c08b6e6f4443853
YERRO-Bruno <brunoyerro@gmail.com>
2024-06-26 10:01:44 -0400

    server3

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn TIPOINTICROIX.wsgi --timeout 120 --workers=3 --threads=3 --worker-connections=1000
-worker: uvicorn uvicorn_worker:app --host 8.8.8.8 --port 8765
+worker: uvicorn TIPOINTICROIX.asgi:main --host 0.0.0.0 --port 8765
--- TIPOINTICROIX/asgi.py
+++ TIPOINTICROIX/asgi.py
@@ -6,11 +6,8 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
+import asyncio
+from server3 import main
 
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TIPOINTICROIX.settings')
-
-application = get_asgi_application()
+if __name__ == "__main__":
+    asyncio.run(main())
