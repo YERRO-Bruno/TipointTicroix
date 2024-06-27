51c40c698c2592ec24ea772f2e3fedf78c4fcf98
YERRO-Bruno <brunoyerro@gmail.com>
2024-06-27 10:48:55 -0400

    server3

--- server3.py
+++ server3.py
@@ -2,10 +2,10 @@ import asyncio
 import websockets
 import socket
 print("debut")
-#s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#s.connect(("8.8.8.8", 80))
-#ipaddress = s.getsockname()[0]
-#print(ipaddress)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+ipaddress = s.getsockname()[0]
+print(ipaddress)
 async def handler(websocket):
     while True:
         message = await websocket.recv()
@@ -14,7 +14,7 @@ async def handler(websocket):
 
 
 async def main():
-    async with websockets.serve(handler, "0.0.0.0", 8765):
+    async with websockets.serve(handler, ipaddress, 8765):
         await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
