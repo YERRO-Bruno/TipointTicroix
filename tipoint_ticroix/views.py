29d771fa644215706c2ef27e492c0577eb6bf7f0
YERRO-Bruno <brunoyerro@gmail.com>
2024-06-27 11:18:10 -0400

    server3

--- tipoint_ticroix/views.py
+++ tipoint_ticroix/views.py
@@ -29,7 +29,8 @@ def test(request):
         ipaddress = s.getsockname()[0]
         settings.SERVEURHOST=ipaddress
         s.close()
-        #with connect("ws://0.0.0.0:8765") as websocket:
+        with connect("ws:"+ipaddress+":8765") as websocket:
+            print("connexion"+"/"+connec[1])
         #   websocket.send("connexion"+"/"+connec[1])
         return render(request, "test.html", context)
     else:
