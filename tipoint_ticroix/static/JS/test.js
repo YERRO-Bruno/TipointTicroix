9173a81c8120a6e573b90b074a1540b13a15ed0a
YERRO-Bruno <brunoyerro@gmail.com>
2024-07-05 15:59:05 -0400

    server3

--- tipoint_ticroix/static/JS/test.js
+++ tipoint_ticroix/static/JS/test.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
 })
 alert("websocket")
 try {
-    var socket = new WebSocket('ws://ti-points-ti-croix.fr:443/ws/chat/');
+    var socket = new WebSocket('ws://ti-points-ti-croix.fr:8765/ws/chat/');
     alert("2")
 } catch (error) {
     alert("Failed to create WebSocket: " + error);
