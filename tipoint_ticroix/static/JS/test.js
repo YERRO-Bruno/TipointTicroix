8cb2f0dc55305dfbb2a3e3e8af99bce4760d74fd
YERRO-Bruno <brunoyerro@gmail.com>
2024-07-08 14:52:01 -0400

    server3

--- tipoint_ticroix/static/JS/test.js
+++ tipoint_ticroix/static/JS/test.js
@@ -31,6 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
     filluserconnecteds()
 })
 alert("debut websock")
+    const io = require('socket.io-client');
     const socket = io.connect('wss://ti-points-ti-croix.fr:8765/ws/chat/', {
     transports: ['websocket']});
     alert("websock1")
