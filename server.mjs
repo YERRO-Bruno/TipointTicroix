8a317be299a34a4a0a973270769ef89402050ecf
YERRO-Bruno <brunoyerro@gmail.com>
2024-07-16 16:23:58 -0400

    node

--- server.mjs
+++ server.mjs
@@ -51,7 +51,7 @@ wss.on('connection', (socket) => {
         if (msg[0]='invite') {
             let socketinvité=""
             tabinvite.push("invite")
-            tabinvite.push(global.connectedUsers(socket))
+            tabinvite.push(global.connectedUsers.get(socket))
             for (var [key, value] of global.connectedUsers) {
                 if (value=msg[1]) {
                     socketinvité=key
