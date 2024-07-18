dbde27eb489e927390ec94f9c0f9e264eae8d968
YERRO-Bruno <brunoyerro@gmail.com>
2024-07-18 18:19:45 -0400

    node

--- server.mjs
+++ server.mjs
@@ -75,7 +75,7 @@ wss.on('connection', (socket) => {
             sockethote.send("accept,"+invité)
         }
         if (msg[0]=='tourjeu') {
-            console.log=(msg[1]+" "+msg[2])
+            console.log=(msg[1])
             let socketadversaire=global.connectedUsers[msg[1]]
             socketadversaire.send("tourjeu,"+msg[2])
         }
