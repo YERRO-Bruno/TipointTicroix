c331f6353f20a6b3649699654f2edfdd952f844a
YERRO-Bruno <brunoyerro@gmail.com>
2024-07-21 17:33:44 -0400

    node

--- tipoint_ticroix/static/JS/test.js
+++ tipoint_ticroix/static/JS/test.js
@@ -99,7 +99,7 @@ document.addEventListener("DOMContentLoaded", function () {
             document.forms["internet"].submit();
         } 
         if (msg[0]=="tourjeu") {
-            print(msg)
+            alert(msg)
             document.getElementById("coup-joueur").value=msg[3]
             document.getElementById("id-etape").value="tourjeu"
             document.getElementById("id-jeton").value="Oui"
