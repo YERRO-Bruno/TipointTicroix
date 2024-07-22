ae09f4d25c0c9ddbbc765770d9264921e890b391
YERRO-Bruno <brunoyerro@gmail.com>
2024-07-22 12:25:47 -0400

    node

--- staticfiles/JS/test.js
+++ staticfiles/JS/test.js
@@ -99,7 +99,6 @@ document.addEventListener("DOMContentLoaded", function () {
             document.forms["internet"].submit();
         } 
         if (msg[0]=="tourjeu") {
-            alert(msg)
             document.getElementById("coup-joueur").value=msg[3]
             document.getElementById("id-etape").value="tourjeu"
             document.getElementById("id-jeton").value="Oui"
