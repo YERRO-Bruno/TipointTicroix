ee232063f03b9d33839e203795652d6c97678ce8
YERRO-Bruno <brunoyerro@gmail.com>
2024-07-23 11:54:56 -0400

    node

--- staticfiles/JS/test.js
+++ staticfiles/JS/test.js
@@ -227,7 +227,6 @@ function displayGameBoard(){
                     }
         }
         if (win != "Non") { 
-            alert("fin manche")
             document.getElementById("ALUI").style.display="none"
             document.getElementById("AVOUS").style.display="none"
             if (document.getElementById("id-finpartie").textContent=="Oui") {
@@ -255,10 +254,10 @@ function displayGameBoard(){
                         //document.getElementById("id-joueur").value=document.getElementById("id-connec").textContent
                         //document.getElementById("id-adversaire").value=document.getElementById("id-adversaire").value
                         document.getElementById("id-match").value="2"
+                        alert(document.getElementById("id-score1").value,document.getElementById("id-score2").value)
                         scorx1=document.getElementById("id-score1").value
                         document.getElementById("id-score1").value=document.getElementById("id-score2").value
                         document.getElementById("id-score2").value=scorx1
-                        alert("avant submit")
                         document.forms["internet"].submit();
                     }
                     count++;
