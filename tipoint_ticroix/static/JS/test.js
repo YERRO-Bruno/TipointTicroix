2d29413ba34093674d5c70ddac2370e404104da3
YERRO-Bruno <brunoyerro@gmail.com>
2024-07-23 10:58:56 -0400

    node

--- tipoint_ticroix/static/JS/test.js
+++ tipoint_ticroix/static/JS/test.js
@@ -242,7 +242,7 @@ function displayGameBoard(){
                 //document.getElementById("id-adversaire").value=document.getElementById("id-adversaire").value
                 document.getElementById("id-match").value=2
                 scorx1=document.getElementById("id-score1").value
-                document.getElementById("id-score1").value=document.getElementById("score2").value
+                document.getElementById("id-score1").value=document.getElementById("id-score2").value
                 document.getElementById("id-score2").value=scorx1
                 document.forms["internet"].submit();
 
