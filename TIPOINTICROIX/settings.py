7e96fa913ae84332737330d402f3c9df45f210c8
YERRO-Bruno <brunoyerro@gmail.com>
2024-08-04 19:05:56 -0400

    correction debug

--- TIPOINTICROIX/settings.py
+++ TIPOINTICROIX/settings.py
@@ -27,7 +27,8 @@ load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG')
+print("DEBUG : ",os.getenv('DEBUG'))
+DEBUG = False
 print(DEBUG)
 
 ALLOWED_HOSTS = ['192.168.1.188', 'localhost', '127.0.0.1',"*","tipointticroix-tipointticroix.*",
