3a07196ce8e72f98fd286a6940e1755fb82eecc6
YERRO-Bruno <brunoyerro@gmail.com>
2024-08-06 16:07:42 -0400

    internet

--- TIPOINTICROIX/settings.py
+++ TIPOINTICROIX/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,6 +140,7 @@ STATICFILES_DIRS = [
 os.path.join(BASE_DIR, "tipoint_ticroix/static")
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
