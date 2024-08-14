9bc61d2843c730876cf200ebfc89dc51fb5f9d56
YERRO-Bruno <brunoyerro@gmail.com>
2024-08-14 09:11:04 -0400

    sprint8

---
+++ tptcinit.sh
@@ -0,0 +1,7 @@
+cd /etc/easypanel/projects/tipointticroix/tipointticroix/code
+sudo npm cache clean --force
+sudo rm -rf node_modules
+sudo npm install
+sudo systemctl daemon-reload
+sudo systemctl enable monservice.service
+sudo systemctl restart monservice.service
