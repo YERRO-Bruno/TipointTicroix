5157a26759ae5e83b1bb08847e14afc71cbd0dc0
YERRO-Bruno <brunoyerro@gmail.com>
2024-08-19 14:30:36 -0400

    modif server.mjs

--- tptcinit.sh
+++ tptcinit.sh
@@ -2,4 +2,5 @@ cd /etc/easypanel/projects/tipointticroix/tipointticroix/code
 npm cache clean --force
 rm -rf node_modules
 npm install
-node server.mjs > tptcinit.log 2>&1
+npm install -g forever
+forever start server.mjs > tptcinit.log 2>&1
