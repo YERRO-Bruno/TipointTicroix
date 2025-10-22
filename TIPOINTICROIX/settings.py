ec5201cdd17104990af3a8f9776fbf8c10f1409c
YERRO-Bruno <brunoyerro@gmail.com>
2025-10-22 12:14:04 -0400

    transfert fichiers images martinique dans les staticfiles

--- TIPOINTICROIX/settings.py
+++ TIPOINTICROIX/settings.py
@@ -86,19 +86,28 @@ ASGI_APPLICATION = 'TIPOINTICROIX.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 import mysql.connector
-DATABASES = {  
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        "NAME": config['NAME'],
-        "USER": config['USER'],
-        "PASSWORD": config['PASSWORD'],
-        "HOST": config['HOST'],
-        'PORT': 3306,  
-        'OPTIONS': {  
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+if DEBUG:
+    # Base de développement (SQLite)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {  
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            "NAME": config['NAME'],
+            "USER": config['USER'],
+            "PASSWORD": config['PASSWORD'],
+            "HOST": config['HOST'],
+            'PORT': 3306,  
+            'OPTIONS': {  
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+            }  
         }  
     }  
-}  
 AUTH_USER_MODEL = 'tipoint_ticroix.User'
 
 # Password validation
